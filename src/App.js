f17c6183afc5747f36b1cbc7583af39ec633c8cf
Nghia <nphan@fandm.edu>
2019-10-03 21:31:35 -0700

    implement login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
+import loginService from './services/login'
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
-  
-  return (
+
+  const loginForm = () => (
     <div>
       <h1>Log in to application</h1>
 
@@ -33,6 +51,26 @@ function App() {
         </div>
         <button type="submit">login</button>
       </form>
+    </div>
+  )
+
+  const blogList = () => (
+    <div>
+      <h1>Blogs</h1>
+      <p>{user.name} logged in</p>
+    </div>
+  )
+
+  return (
+    <div>
+
+      <h3>{errorMessage}</h3>
+
+      {user === null ?
+        loginForm() :
+        blogList()
+      }
+      
     </div>
   );
 }
