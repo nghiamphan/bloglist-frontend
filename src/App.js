f4014d9e0dc85787b91d839dfbb1a75098620a96
Nghia <nphan@fandm.edu>
2019-10-03 20:02:55 -0700

    update dependencies, add login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+  }
+  
   return (
     <div>
       <h1>Log in to application</h1>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
     </div>
   );
 }
