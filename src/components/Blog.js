e328c431630c47cba077d3ea1e590dbc0a249868
Nghia <nphan@fandm.edu>
2019-10-10 02:38:51 -0700

    blog item can be toggled to show/hide more details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <li className="blog">
-    {blog.title}, by <i>{blog.author}</i>
-  </li>
-)
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
+  if (expanded) {
+    return (
+      <li className="blog-expanded">
+        <div onClick={() => toggleExpanded()}>
+          {blog.title}, by <i>{blog.author}</i>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes} likes
+          <button>like</button>
+        </div>
+        <div>added by {blog.user.name}</div>
+      </li>
+    )
+  }
+
+  return (
+    <li className="blog" onClick={() => toggleExpanded()}>
+      {blog.title}, by <i>{blog.author}</i>
+    </li>
+  )
+}
 
 export default Blog
